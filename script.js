0f61d614b90f5cc556b89899fae119ad70439031
nodilod <93993213+nodilod@users.noreply.github.com>
2022-03-27 13:04:25 +0200

    v2

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
     buttonNo.addEventListener("mouseover", function () {
         console.log("no");
         //randam number betwin 0 and 100
-        let randomNumberX = Math.floor(Math.random() * 100);
+        let randomNumberX = Math.floor(Math.random() * 80);
         let randomNumberY = Math.floor(Math.random() * 80);
         console.log(randomNumberX ,"  ", randomNumberY);
 
         //chane the position of the box
         let box = document.getElementById("questionBox");
-        box.style.marginTop = randomNumberY + "%";
-        box.style.marginLeft = randomNumberX + "%";
+        box.style.marginTop = randomNumberY + "px";
+        box.style.marginLeft = randomNumberX + "px";
     });
 });
